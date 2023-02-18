0476c2e2f032adbf9c97bd6a69217b46416aea1f
Roberto Pitarch <roberto.i12@icloud.com>
2023-02-18 11:50:25 +0100

    Added sendPhoto example

--- listener.py
+++ listener.py
@@ -1,6 +1,8 @@
 from telegram import *
 from telegram.ext import *
 
+from time import sleep
+
 
 def listener(update: Update, context: CallbackContext):
     bot = context.bot
@@ -14,3 +16,8 @@ def listener(update: Update, context: CallbackContext):
 
     if priv and 'hola' in message.lower():
         update.message.reply_text('¡Hola! ¿Conoces a @HanzoBot?')
+
+    if 'gato' in message.lower():
+        bot.send_chat_action(id, action=ChatAction.UPLOAD_PHOTO)
+        sleep(1)
+        bot.sendPhoto(chat_id=id, photo='https://i.ibb.co/Dp4dqJC/IMG-7920.jpg')
